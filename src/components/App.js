d86ea6b325769a800e675a1ecff6a3a8df1dccb2
Swathi Gopavarapu <swathigopavarapu1@gmail.com>
2025-09-18 18:24:02 +0530

    initial state - golf react

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,58 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import '../styles/App.css';
-
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderBall: false,
+      posi: 0,
+      ballPosition: { left: "50%", top: "10%"}
     };
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
+  }
 
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
-    }
+  buttonClickHandler() {
+    this.setState(
+      { renderBall: true, posi: 0, ballPosition: { left: "50%", top: "10%" } },
+      () => {
+        let steps = 0;
+        this.ballInterval = setInterval(() => {
+          steps++;
+          if (steps >= 20) { 
+            clearInterval(this.ballInterval);
+          }
+          this.setState(prevState => {
+            const newPos = prevState.posi + 5;
+            return { 
+              posi: newPos, 
+              ballPosition: { left: `calc(50% + ${newPos}px)`, top: "10%"
+            }
+            }
+          });
+        }, 100); // move every 100ms
+      }
+    );
+  }
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
-    }
+  componentWillUnmount() {
+    clearInterval(this.ballInterval);
+  }
 
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
+  renderBallOrButton() {
+    if (this.state.renderBall) {
+      return <div className="ball" style={this.state.ballPosition}></div>
+    } else {
+      return <button className="start" onClick={this.buttonClickHandler}>Start</button>
     }
+  }
+
+  render() {
+    return (
+      <div className="playground">
+        {this.renderBallOrButton()}
+      </div>
+    )
+  }
 }
-
 
 export default App;
