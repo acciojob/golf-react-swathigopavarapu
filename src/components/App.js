9966c9fea548942848d7c5ccc6994d220ba411f2
Swathi Gopavarapu <swathigopavarapu1@gmail.com>
2025-09-18 18:31:23 +0530

    initial state - golf

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,37 @@
 import React, { Component } from "react";
 import '../styles/App.css';
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       renderBall: false,
       posi: 0,
-      ballPosition: { left: "50%", top: "10%"}
+      ballPosition: { left: "50%", top: "10%" }
     };
     this.buttonClickHandler = this.buttonClickHandler.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("keydown", this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.handleKeyDown);
+    clearInterval(this.ballInterval);
+  }
+
+   handleKeyDown(e) {
+    if (!this.state.renderBall) return;
+    if (e.key === "ArrowRight") {
+      this.setState(prevState => {
+        const newPos = prevState.posi + 5;
+        return {
+          posi: newPos,
+          ballPosition: { left: `calc(50% + ${newPos}px)`, top: "10%" }
+        };
+      });
+    }
   }
 
   buttonClickHandler() {
@@ -18,31 +41,26 @@ class App extends Component {
         let steps = 0;
         this.ballInterval = setInterval(() => {
           steps++;
-          if (steps >= 20) { 
+          if (steps >= 20) {
             clearInterval(this.ballInterval);
           }
           this.setState(prevState => {
             const newPos = prevState.posi + 5;
-            return { 
-              posi: newPos, 
-              ballPosition: { left: `calc(50% + ${newPos}px)`, top: "10%"
-            }
-            }
+            return {
+              posi: newPos,
+              ballPosition: { left: `calc(50% + ${newPos}px)`, top: "10%" }
+            };
           });
-        }, 100); // move every 100ms
+        }, 100); 
       }
     );
   }
 
-  componentWillUnmount() {
-    clearInterval(this.ballInterval);
-  }
-
   renderBallOrButton() {
     if (this.state.renderBall) {
-      return <div className="ball" style={this.state.ballPosition}></div>
+      return <div className="ball" style={this.state.ballPosition}></div>;
     } else {
-      return <button className="start" onClick={this.buttonClickHandler}>Start</button>
+      return <button className="start" onClick={this.buttonClickHandler}>Start</button>;
     }
   }
 
@@ -51,7 +69,7 @@ class App extends Component {
       <div className="playground">
         {this.renderBallOrButton()}
       </div>
-    )
+    );
   }
 }
 
